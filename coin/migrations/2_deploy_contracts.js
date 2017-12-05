42e5bf7062ddd50ef3ea83f223db27c29fec603a
Petro <petrocoin@protonmail.com>
2017-12-05 01:16:35 -0500

    Petro docs

--- coin/migrations/2_deploy_contracts.js
+++ coin/migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
 const PetroSale = artifacts.require("./PetroCrowdsale.sol")
 
 module.exports = function(deployer, network, accounts) {
-    const startTime = 1512447026 + 600 // 12/05/2017 @ 3:45am (UTC)
+    const startTime = 1512454081 + 600 // 12/05/2017 @ 3:45am (UTC)
     const endTime = startTime + (86400 * 30)
     const rate = new web3.BigNumber(100000)
     const wallet = "0x5d693B6E328CB4c5751015b9c0257ae8F76d5F6D"
